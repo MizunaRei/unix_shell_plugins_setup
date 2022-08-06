850f31bc6a336f67436647ffeacbdd72527ce0e9
MoonLightElf <dr_liu0806@msn.com>
2022-08-07 08:35:29 +0900

    update unix_shell_plugins_setup.sh

--- unix_shell_plugins_setup.sh
+++ unix_shell_plugins_setup.sh
@@ -2,7 +2,7 @@
 
 
 ## global variables
-SHELL_REQUIREMENTS = "$2"
+SHELL_REQUIREMENTS = "$PWD"
 SHELL_REQUIREMENTS = 'make gawk curl git'
 UNIX_SHELL_CONTRACTION = "a"
 UNIX_SHELL_NAME = "bash fish zsh"
