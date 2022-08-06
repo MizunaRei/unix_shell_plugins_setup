a6338efe91e01634b3443905b42a1ad2841147e4
MoonLightElf <dr_liu0806@msn.com>
2022-08-07 08:18:24 +0900

    update unix_shell_plugins_setup.sh

--- unix_shell_plugins_setup.sh
+++ unix_shell_plugins_setup.sh
@@ -2,158 +2,158 @@
 
 
 ## global variables
-SHELL_REQUIREMENTS="make gawk curl git"
-UNIX_SHELL_CONTRACTION="a"
-UNIX_SHELL_NAME="bash fish zsh"
+SHELL_REQUIREMENTS = "make gawk curl git"
+UNIX_SHELL_CONTRACTION = "a"
+UNIX_SHELL_NAME = "bash fish zsh"
 
 
 
-function main(){
-## same as main() function in C language
+function main() {
+	## same as main() function in C language
 
-## save user's current working directory
-LAST_WORKING_DIRECTORY=$PWD
-## go to user home directory for default setup.
-cd ~
+	## save user's current working directory
+	LAST_WORKING_DIRECTORY = $PWD
+	## go to user home directory for default setup.
+	cd ~
 
-## choose unix shells to setup
-read -p " \n Which Unix shell do you use? \n A. all of three shells \n B. bash \n F. fish \n Z. zsh \n \n Q. cancel setup and quit.  \n "  -n 1 -r unix_shell_contraction
-echo
-if
-	[[ $unix_shell_contraction =~ ^[Aa]$ ]]
-	
+	## choose unix shells to setup
+	read -p " \n Which Unix shell do you use? \n A. all of three shells \n B. bash \n F. fish \n Z. zsh \n \n Q. cancel setup and quit.  \n " -n 1 -r unix_shell_contraction
+	echo
+	if
+		[[$unix_shell_contraction =~ ^[Aa]$]]
+
 	then
-	 unix_shell_name="bash fish zsh"
-fi
+	unix_shell_name = "bash fish zsh"
+	fi
 
-if
-	[[ $unix_shell_contraction =~ ^[Bb]$ ]]
+	if
+		[[$unix_shell_contraction =~ ^[Bb]$]]
 	then
-	 unix_shell_name="bash"
-fi
+	unix_shell_name = "bash"
+	fi
 
-if
-	[[ $unix_shell_contraction =~ ^[Ff]$ ]]
-	then 
-	unix_shell_name="fish"
-fi
+	if
+		[[$unix_shell_contraction =~ ^[Ff]$]]
+	then
+	unix_shell_name = "fish"
+	fi
 
-if
-	[[ $unix_shell_contraction =~ ^[Zz]$ ]]
-	then 
-	unix_shell_name="zsh"
-fi
+	if
+		[[$unix_shell_contraction =~ ^[Zz]$]]
+	then
+	unix_shell_name = "zsh"
+	fi
 
-if
-	[[ $unix_shell_contraction =~ ^[Qq]$ ]]
-	then 
+	if
+		[[$unix_shell_contraction =~ ^[Qq]$]]
+	then
 	cd "$last_working_directory"
 	echo "Nothing changed. If you like this script, please give it a star. "
 	exit
-fi
+	fi
 
-distribution_detecting_shells_setup 
+	distribution_detecting_shells_setup
 
-## exiting
-cd "$last_working_directory"
-echo "Setup completed. If you like this script, please give it a star. "
-exit
+	## exiting
+	cd "$last_working_directory"
+	echo "Setup completed. If you like this script, please give it a star. "
+	exit
 }
 
 
-function bash_setup(){
-git clone --recursive --depth=1 https://github.com/akinomyoga/ble.sh.git
-make -C ble.sh install PREFIX=~/.local
-curl -OL https://github.com/MoonLightElf/unix_shell_plugins_setup/raw/main/bash_plugins/ble.sh_bashrc_default
-cat ble.sh_bashrc_default  >> ~/.bashrc
-rm ./ble.sh_bashrc_default
-curl -OL https://bashhub.com/setup && bash setup
-rm ./setup
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
-rm ./install.sh
+function bash_setup() {
+	git clone --recursive --depth = 1 https://github.com/akinomyoga/ble.sh.git
+	make -C ble.sh install PREFIX=~/.local
+	curl -OL https://github.com/MoonLightElf/unix_shell_plugins_setup/raw/main/bash_plugins/ble.sh_bashrc_default
+	cat ble.sh_bashrc_default >> ~/.bashrc
+	rm ./ble.sh_bashrc_default
+	curl -OL https://bashhub.com/setup && bash setup
+	rm ./setup
+	bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
+	rm ./install.sh
 }
 
-function fish_setup(){
-curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
-rm ./install
+function fish_setup() {
+	curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+	rm ./install
 
 }
 
-function zsh_setup(){
-	curl -OL  https://github.com/MoonLightElf/unix_shell_plugins_setup/raw/main/zsh_plugins/zsh_plugins_setup.sh | zsh
+function zsh_setup() {
+	curl -OL https://github.com/MoonLightElf/unix_shell_plugins_setup/raw/main/zsh_plugins/zsh_plugins_setup.sh | zsh
 	## chmod +x  zsh_plugins_setup.sh
 	## /bin/zsh  zsh_plugins_setup.sh
 	rm ./zsh_plugins_setup.sh
 }
 
 
-function shells_setup(){
-if [[ "$unix_shell_name"== *"bash"* ]]; then
-  echo " \n Setting up bash plugins. \n "
-  bash_setup
-fi
+function shells_setup() {
+	if [["$unix_shell_name" == *"bash"*]] then
+	echo " \n Setting up bash plugins. \n "
+	bash_setup
+	fi
 
-if [[ "$unix_shell_name"== *"fish"* ]]; then
-  echo " \n Setting up fish plugins. \n "
-  fish_setup
-fi
+	if [["$unix_shell_name" == *"fish"*]] then
+	echo " \n Setting up fish plugins. \n "
+	fish_setup
+	fi
 
-if [[ "$unix_shell_name"== *"zsh"* ]]; then
-  echo " \n Setting up zsh plugins. \n "
-  zsh_setup
-fi
+	if [["$unix_shell_name" == *"zsh"*]] then
+	echo " \n Setting up zsh plugins. \n "
+	zsh_setup
+	fi
 }
 
 
-function  distribution_detecting_shells_setup(){
-if
-	"Darwin"==`uname` || "darwin"==`uname`
-then 
+function distribution_detecting_shells_setup() {
+	if
+		"Darwin"==`uname` || "darwin"==`uname`
+	then
 	# brew.sh officaial installer script
-	 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 	# curl -OL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
 	# chmod +x ./install.sh
-	# /bin/bash -c install.sh 
+	# /bin/bash -c install.sh
 	brew install make gawk curl git "$unix_shell_name"
 	rm ./install.sh
 	shells_setup
-elif 
-	[[ "alpine"==`uname` || "Alpine"==`uname`  || `uname`== *"alpine"* || `uname`== *"Alpine"*   ]]
-then
+	elif
+	[["alpine"==`uname` || "Alpine"==`uname` || `uname`== *"alpine"* || `uname`== *"Alpine"*]]
+	then
 	apk add make gawk curl git "$unix_shell_name"
 	shells_setup
-elif
-	[[ "debian"==`uname` || "ubuntu"==`uname` || "Debian"==`uname` || "Ubuntu"==`uname`  || `uname`== *"Debian"* || `uname`== *"Ubuntu"*  || `uname`== *"debian"*  ||  `uname`== *"ubuntu"*  || `uname`== *"Termux"* || `uname`== *"termux"*  ]]
-then
+	elif
+	[["debian"==`uname` || "ubuntu"==`uname` || "Debian"==`uname` || "Ubuntu"==`uname` || `uname`== *"Debian"* || `uname`== *"Ubuntu"* || `uname`== *"debian"* || `uname`== *"ubuntu"* || `uname`== *"Termux"* || `uname`== *"termux"*]]
+	then
 	apt install make gawk curl git "$unix_shell_name"
 	shells_setup
-elif
-	[[ "Fedora"==`uname` || "fedora"==`uname` || `uname`== *"Fedora"*  ||  `uname`== *"fedora"*  || `uname`== *[cC][eE][nN][tT]*  || `uname`== *"cent"* ||  `uname`== *"Cent"*  || `uname`== *"RHEL"*  ]]
+	elif
+	[["Fedora"==`uname` || "fedora"==`uname` || `uname`== *"Fedora"* || `uname`== *"fedora"* || `uname`== *[cC][eE][nN][tT]* || `uname`== *"cent"* || `uname`== *"Cent"* || `uname`== *"RHEL"*]]
 	then
 	dnf install make gawk curl git "$unix_shell_name"
 	shells_setup
-elif 
-	[[ `uname`== *"suse"*  || `uname`== *"SUSE"* || `uname`== *"SLE"*  || "OpenSUSE"==`uname` || "opensuse"==`uname`  ]]  
-then
+	elif
+	[[`uname`== *"suse"* || `uname`== *"SUSE"* || `uname`== *"SLE"* || "OpenSUSE"==`uname` || "opensuse"==`uname`]]
+	then
 	zypper install make gawk curl git "$unix_shell_name"
 	shells_setup
-elif 
-	[[ "Arch"==`uname` || "manjaro"==`uname`  || `uname`== *"arch"* || `uname`== *"manjaro"*   ]]
+	elif
+	[["Arch"==`uname` || "manjaro"==`uname` || `uname`== *"arch"* || `uname`== *"manjaro"*]]
 	then
 	pacman -S make gawk curl git "$unix_shell_name"
 	shells_setup
-else 
-read -p " \n We could not determine what distribution you are running. Continue installation anyway? \n  "  -n 1 -r REPLY
-echo
-	if [[  $REPLY == ^[Yy]$ ]]
-	then
-		shells_setup
 	else
-	cd "$last_working_directory"
+		read -p " \n We could not determine what distribution you are running. Continue installation anyway? \n  " -n 1 -r REPLY
+	echo
+	if [[$REPLY == ^[Yy]$]]
+	then
+	shells_setup
+	else
+		cd "$last_working_directory"
 	echo "Nothing changed. If you like this script, please give it a star. "
 	exit
 	fi
-fi
+	fi
 }
 
 
