8dbd9c259c2c6d745e0eac45c9773c08c9eca2e7
liu <dr_liu0806@msn.com>
2022-10-05 22:30:45 +0900

    test if fish , gawk, make exist.

--- unix_shell_plugins_setup.sh
+++ unix_shell_plugins_setup.sh
@@ -17,67 +17,100 @@ function main() {
 
 
 	## greetings
-	 printf greetings
+	 printf " \n Welcome. This script could help you with setting up some awesome plugins for bash, zsh, and fish shell. For more information please read software homepage. \n \n https://github.com/MilkyMAISHIRANUI/unix_shell_plugins_setup \n \n "
 	## echo -e ## I do not know what this command do.
 	
 
 	## zsh-autocomlete setup. start.
 	printf " \n Would you like to install marlonrichert/zsh-autocomplete ? [y/n] \n It is real-time type-ahead completion for Zsh. Asynchronous find-as-you-type autocompletion. \n "
-	read -k1 REPLY
-	if [[ "$REPLY"=~^[Yy]$ ]]
-	then
-		zsh-autocomplete-setup
-	fi
-	## zsh-autocomplete setup. EOF.
+	read answer
+ # if echo "$answer" | grep -iq "^y" ;then
+ if [ "$answer" != "${answer#[Yy]}" ] ;then # this grammar (the #[] operator) means that the variable $answer where any Y or y in 1st position will be dropped if they exist.
+    #echo Yes
+    zsh-autocomplete-setup
+else
+    #echo No
+    printf " \n Skipped marlonrichert/zsh-autocomplete installation. \n "
+fi
+## zsh-autocomplete setup. End.
 
 
-	printf " \n Would you like to install ohmyzsh/ohmyzsh ? \n It is a delightful community-driven framework for managing your zsh configuration. Includes 300+ optional plugins,140+ themes. \n "
-	read -k1 REPLY
-	if [[ "$REPLY"=~^[Yy]$ ]]
-	then
-		ohmyzsh-setup
-	fi
+	# ohmyzsh setup. start.
+	printf " \n Would you like to install ohmyzsh/ohmyzsh ? [y/n]  \n It is a delightful community-driven framework for managing your zsh configuration. Includes 300+ optional plugins,140+ themes. \n "
+	read answer
+ # if echo "$answer" | grep -iq "^y" ;then
+ if [ "$answer" != "${answer#[Yy]}" ] ;then # this grammar (the #[] operator) means that the variable $answer where any Y or y in 1st position will be dropped if they exist.
+    #echo Yes
+    ohmyzsh-setup
+else
+    #echo No
+    printf " \n Skipped ohmyzsh/ohmyzsh installation. \n "
+fi
+	# ohmyzsh setup. ENd.
+	
+	
+	# powerlevel10k setup. start.
+	printf " \n Would you like to install romkatv/powerlevel10k ? [y/n]  \n It is a theme for Zsh. It emphasizes speed, flexibility and out-of-the-box experience. \n "
+	read answer
+ # if echo "$answer" | grep -iq "^y" ;then
+ if [ "$answer" != "${answer#[Yy]}" ] ;then # this grammar (the #[] operator) means that the variable $answer where any Y or y in 1st position will be dropped if they exist.
+    #echo Yes
+    powerlevel10k-setup
+else
+    #echo No
+    printf " \n Skipped romkatv/powerlevel10k installation. \n "
+fi
+	# powerlevel10k setup. End.
+	
+	# oh-my-bash setup . Start.
+	printf " \n Would you like to install ohmybash/oh-my-bash ? [y/n]  \n It is a delightful community-driven framework for managing your bash configuration. \n "
+	read answer
+ # if echo "$answer" | grep -iq "^y" ;then
+ if [ "$answer" != "${answer#[Yy]}" ] ;then # this grammar (the #[] operator) means that the variable $answer where any Y or y in 1st position will be dropped if they exist.
+    #echo Yes
+    oh-my-bash-setup
+else
+    #echo No
+    printf " \n Skipped ohmybash/oh-my-bash installation. \n "
+fi
+	# oh-my-bash setup. End.
 
 
-	printf " \n Would you like to install romkatv/powerlevel10k ? \n It is a theme for Zsh. It emphasizes speed, flexibility and out-of-the-box experience. \n "
-	read -k1 REPLY
-	if [[ "$REPLY"=~^[Yy]$ ]]
-	then
-		powerlevel10k-setup
-	fi
+	# ble.sh setup. start.
+	printf " \n Would you like to install akinomyoga/ble.sh ? [y/n]  \n It is a full-featured line editor. Syntax highlighting, auto suggestions, vim modes, etc. are available in Bash interactive sessions. \n "
+	read answer
+ # if echo "$answer" | grep -iq "^y" ;then
+ if [ "$answer" != "${answer#[Yy]}" ] ;then # this grammar (the #[] operator) means that the variable $answer where any Y or y in 1st position will be dropped if they exist.
+    #echo Yes
+    ble-sh-setup
+else
+    #echo No
+    printf " \n Skipped akinomyoga/ble.sh installation. \n "
+fi
+	 ble.sh setup. End.
+	 
 
+	## oh-my-fish setup. start.
+	printf " \n Would you like to install oh-my-fish/oh-my-fish ? [y/n]  \n It provides core infrastructure to allow you to install packages which extend or modify the look of your shell. It's fast, extensible and easy to use. \n "
+	read answer
+ # if echo "$answer" | grep -iq "^y" ;then
+ if [ "$answer" != "${answer#[Yy]}" ] ;then # this grammar (the #[] operator) means that the variable $answer where any Y or y in 1st position will be dropped if they exist.
+    #echo Yes
+    oh-my-fish-setup
+else
+    #echo No
+    printf " \n Skipped oh-my-fish/oh-my-fish installation. \n "
+fi
+	# oh-my-fish setup. End.
+	
 
-	printf " \n Would you like to install ohmybash/oh-my-bash ? \n It is a delightful community-driven framework for managing your bash configuration. \n "
-	read -k1 REPLY
-	if [[ "$REPLY"=~^[Yy]$ ]]
-	then
-		oh-my-bash-setup
-	fi
-
-
-	printf " \n Would you like to install akinomyoga/ble.sh ? \n It is a full-featured line editor. Syntax highlighting, auto suggestions, vim modes, etc. are available in Bash interactive sessions. \n "
-	read -k1 REPLY
-	if [[ "$REPLY"=~^[Yy]$ ]]
-	then
-		ble-sh-setup
-	fi
-
-
-	printf " \n Would you like to install oh-my-fish/oh-my-fish ? \n It provides core infrastructure to allow you to install packages which extend or modify the look of your shell. It's fast, extensible and easy to use. \n "
-	read -k1 REPLY
-	if [[ "$REPLY"=~^[Yy]$ ]]
-	## if statement failed.
-	then
-		## printf "$REPLY"
-		oh-my-fish-setup
-	fi
-
-	exit_cleanup
+	## clean up before exiting this script.
+	exiting_cleanup
 }
 
 
-function exit_cleanup() {
-	printf " \n Setup completed. \n If you like this script, please give it a star on its home page. \n "
+function exiting_cleanup() {
+	printf " \n Setup completed. \n  Open a new terminal window to enjoy it. If unix shell went wrong, please follow this guide. \n  https://github.com/marlonrichert/zsh-autocomplete#manual-installation  \n You may try to rename or delete dotfiles such as ~/.zshrc , ~/.zprofile , ~/.p10k.zsh , and the folder of plugins (default path is ~/zsh-snap ) . \n Then execute this script again.  \n If you like this script, please give it a star on its home page. \n "
 	rm ./unix_shell_plugins_setup.sh
 	cd "$last_working_directory"
 	exit 0
@@ -87,37 +120,59 @@ function exit_cleanup() {
 
 
 function oh-my-fish-setup(){
-
-	if [[ `which fish`=1 ]]
-	## if statement failed.
-	then
-		printf " \n We could not find fish.  \n Please install fish by package manager. \n "
-	else			
+printf "\n Setting up oh-my-fish/oh-my-fish . \n "
+	## if [ "$(which fish)"=1 ]
+	## above if statement failed.
+	if [ -e "$(which fish)" ]; then
+	
+		
+				
 	## oh-my-fish official installer
+		printf " \n Please press Y to install over existing installation. \n "
 		curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
 		rm ./install
+		printf " \n oh-my-fish/oh-my-fish was installed. \n "
+	else
+	printf " \n We could not find fish shell binary file.  \n Please install fish shell by package manager i.e. Homebrew. \n "
 	fi
+	
 }
 
 
 function ble-sh-setup() {
-	## ble.sh official installer
-	git clone --recursive --depth=1 https://github.com/akinomyoga/ble.sh.git
+	printf " \n Setting up akinomyoga/ble.sh . \n "
+	## detect gawk make
+	if [ -e "$(which gawk)" ]; then
+		if [ -e "$(which make)" ]; then
+			printf " \n NOTE: Please visit software project homepage for installation guide.  \n https://github.com/akinomyoga/ble.sh \n "
+			## ble.sh official installer
+		git clone --recursive --depth=1 https://github.com/akinomyoga/ble.sh.git
 	make -C ble.sh install PREFIX=~/.local
 	curl -OL https://github.com/MilkyMAISHIRANUI/unix_shell_plugins_setup/raw/main/bash_plugins/ble.sh_bashrc_default
 	cat ble.sh_bashrc_default >> ~/.bashrc
 	rm ./ble.sh_bashrc_default
+	printf " \n akinomyoga/ble.sh was installed. \n "
+		else
+			printf " \n We could not find make ( GNU make ) binary file.  \n Please install make by package manager i.e. Homebrew. \n " 
+		fi
+	else 
+		printf " \n We could not find gawk ( GNU awk ) binary file.  \n Please install gawk by package manager i.e. Homebrew. \n "
+	fi
+	
 }
 
 
 function oh-my-bash-setup(){
+	printf " \n Setting up ohmybash/oh-my-bash . \n "
 	## oh-my-bash official installer
 	bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
 	rm ./install.sh
+	printf " \n ohmybash/oh-my-bash was installed. \n "
 }
 
 
 function powerlevel10k-setup(){
+printf " \n Setting up romkatv/powerlevel10k . \n "
 	## install powerlevel10k into ohmyzsh. start
 ## Install powerlevel10k into ohmyzsh so it can be used as a random theme.
 znap source romkatv/powerlevel10k
@@ -132,22 +187,26 @@ printf " \n \n Use this command to use powerlevel10k theme. \n Or write this com
 printf "        znap prompt ohmyzsh/ohmyzsh powerlevel10k"
 printf " \n \n Use this command to use a random theme. \n Or write this command into ~/.zshrc file. \n \n"
 printf "        znap prompt ohmyzsh/ohmyzsh random"
-## install powerlevel10k into ohmyzsh. EOF
+printf " \n romkatv/powerlevel10k was installed. \n "
+## install powerlevel10k into ohmyzsh. End
 }
 
 
 function ohmyzsh-setup() {
+printf " \n Setting up ohmyzsh/ohmyzsh. \n "
 	## install ohmyzsh via zsh-snap. start
 ## Install ohmyzsh outside zsh-snap may cause compatibility issues between ohmyzsh and zsh-snap. 
 ## Use this command to execute desired plugins of ohmyzsh.
 znap source ohmyzsh/ohmyzsh lib/{git,theme-and-appearance}
-printf 'znap source ohmyzsh/ohmyzsh lib/{git,theme-and-appearance}' >>~/.zshrc
+printf 'znap source ohmyzsh/ohmyzsh lib/{git,theme-and-appearance}' >> ~/.zshrc
 ## Path to your oh-my-zsh installation.
-printf 'export ZSH="$HOME/zsh-snap/ohmyzsh" ' >>~/.zshrc
-## install ohmyzsh via zsh-snap. EOF
+printf 'export ZSH="$HOME/zsh-snap/ohmyzsh" ' >> ~/.zshrc
+printf " \n ohmyzsh/ohmyzsh was installed. \n "
+## install ohmyzsh via zsh-snap. End
 }
 
 function zsh-autocomplete-setup() {
+printf " \n Setting up marlonrichert/zsh-autocomplete. \n "
 ## copy zsh plugins  default settings to ~/.zshrc 
 	curl -o zshrc_default  -sL  https://github.com/MilkyMAISHIRANUI/unix_shell_plugins_setup/raw/main/zsh_plugins/zshrc_default
 cat zshrc_default >> ~/.zshrc
@@ -159,9 +218,11 @@ printf " \n \n Please keep the default installation directory unless you know wh
 git clone --depth 1 -- https://github.com/marlonrichert/zsh-snap.git ~/zsh-snap/zsh-snap
 chmod +x ~/zsh-snap/zsh-snap/znap.zsh
 source ~/zsh-snap/zsh-snap/znap.zsh
-## use zshrc-default instead of echo zshrc.
-## echo 'source ~/zsh-snap/zsh-snap/znap.zsh'>>~/.zshrc
-## install marlonrichert/zsh-snap and auto-complete. EOF
+## use zshrc-default instead of echo >> zshrc.
+## echo 'source ~/zsh-snap/zsh-snap/znap.zsh' >> ~/.zshrc
+## zshrc-default includes zsh-autocomplete.
+printf " \n marlonrichert/zsh-autocomplete was installed. \n "
+## install marlonrichert/zsh-snap and auto-complete. End
 }
 
 
