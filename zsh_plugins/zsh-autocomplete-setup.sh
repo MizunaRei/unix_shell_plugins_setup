be5b73f53ab0df56d1b7420cbf73067be3d449b4
MilkyMAISHIRANUI <dr_liu0806@msn.com>
2022-11-13 11:04:08 +0900

    improve fish-like autocomplete

---
+++ zsh_plugins/zsh-autocomplete-setup.sh
@@ -0,0 +1,6 @@
+## enable fish-like autocomplete by zsh-snap. start.
+	 znap source marlonrichert/zsh-autocomplete 
+	 znap source zsh-users/zsh-autosuggestions
+	 znap source zsh-users/zsh-completions
+	 znap source zsh-users/zsh-syntax-highlighting
+## enable fish-like autocomplete by zsh-snap. end.
